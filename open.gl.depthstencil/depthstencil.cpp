eee654d1f700dcc441055a47e98e80f64b539c3a
Chenghai Zhang <nogains.nopains@gmail.com>
2014-07-12 09:59:10 -0500

    add more comments for the depthstencil example.

--- open.gl.depthstencil/depthstencil.cpp
+++ open.gl.depthstencil/depthstencil.cpp
@@ -215,15 +215,22 @@ int main( void )
         glEnable(GL_STENCIL_TEST);
 
         // Draw floor
+        // Normallly we need disable both the depth buffer and color buffer,
+        // but in this case, we want to write the color buffer, so don't do it
+        // here, otherwise, the floor will not be draw properly!
+        //glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); // disable colro buffer.
+        glDepthMask(GL_FALSE); // Don't write to depth buffer
         glStencilFunc(GL_ALWAYS, 1, 0xFF); // Set any stencil to 1
         glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
         glStencilMask(0xFF); // Write to stencil buffer
-        glDepthMask(GL_FALSE); // Don't write to depth buffer
         glClear(GL_STENCIL_BUFFER_BIT); // Clear stencil buffer (0 by default)
 
         glDrawArrays(GL_TRIANGLES, 36, 6);
 
         // Draw cube reflection
+        // Normally we need to reenable the color buffer writting, but
+        // in this case, we did nothing to it, so no need to reneable it.
+        //glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
         glStencilFunc(GL_EQUAL, 1, 0xFF); // Pass test if stencil value is 1
         glStencilMask(0x00); // Don't write anything to stencil buffer
         glDepthMask(GL_TRUE); // Write to depth buffer
